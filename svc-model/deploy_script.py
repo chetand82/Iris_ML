cb221896f3cd339b3ac69722a2ed6b6fbe6733c9
chetan <chetan.d82@gmail.com>
2024-12-15 19:20:54 +0530

    new changes to fix deploy script failures

--- svc-model/deploy_script.py
+++ svc-model/deploy_script.py
@@ -8,10 +8,14 @@ sagemaker_session = sagemaker.Session()
 sklearn_estimator = SKLearn(
     entry_point='model/train.py',
     role='arn:aws:iam::430238166084:role/chetan_oidc_aws_github',
+    max_run=60,
     instance_count=1,
-    instance_type='ml.t3.medium',
+    instance_type='ml.t3.large',
+    use_spot_instances=True,
+    max_wait=120,
+    max_retry_attempts=1,
     framework_version='0.23-1',
-    py_version='py3',
+    py_version='py3',    
     hyperparameters={
         'kernel': 'rbf',
         'C': 1.0,
@@ -28,5 +32,5 @@ sklearn_estimator.fit({
 # Deploy the model
 predictor = sklearn_estimator.deploy(
     initial_instance_count=1,
-    instance_type='ml.t3.medium'
+    instance_type='ml.t3.large'
 )
